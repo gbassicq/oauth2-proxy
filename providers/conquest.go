017747bc94a8ecd6113ccd01d59f0d9a5ebc5401
gbassi <gbassi@conquestsoftware.com.au>
2021-08-05 15:00:39 +0930

    Update: Added a basic working config and fixed provider

--- providers/conquest.go
+++ providers/conquest.go
@@ -9,7 +9,7 @@ import (
 	"net/url"
 	"time"
 
-	// "github.com/bitly/go-simplejson"
+	"github.com/bitly/go-simplejson"
 	"github.com/oauth2-proxy/oauth2-proxy/v7/pkg/apis/sessions"
 	"github.com/oauth2-proxy/oauth2-proxy/v7/pkg/logger"
 	"github.com/oauth2-proxy/oauth2-proxy/v7/pkg/requests"
@@ -18,7 +18,7 @@ import (
 // ConquestProvider represents an Conquest based Identity Provider
 type ConquestProvider struct {
 	*ProviderData
-	Tenant string
+	// Tenant string
 }
 
 var _ Provider = (*ConquestProvider)(nil)
@@ -55,10 +55,10 @@ var (
 
 	// Default ProtectedResource URL for Conquest.
 	// Pre-parsed URL of https://graph.microsoft.com.
-	conquestDefaultProtectResourceURL = &url.URL{
-		Scheme: "https",
-		Host:   "graph.microsoft.com",
-	}
+	// conquestDefaultProtectResourceURL = &url.URL{
+	// 	Scheme: "https",
+	// 	Host:   "graph.microsoft.com",
+	// }
 )
 
 // NewConquestProvider initiates a new ConquestProvider
@@ -72,30 +72,30 @@ func NewConquestProvider(p *ProviderData) *ConquestProvider {
 		scope:       conquestDefaultScope,
 	})
 
-	if p.ProtectedResource == nil || p.ProtectedResource.String() == "" {
-		p.ProtectedResource = conquestDefaultProtectResourceURL
-	}
+	// if p.ProtectedResource == nil || p.ProtectedResource.String() == "" {
+	// 	p.ProtectedResource = conquestDefaultProtectResourceURL
+	// }
 	if p.ValidateURL == nil || p.ValidateURL.String() == "" {
 		p.ValidateURL = p.ProfileURL
 	}
 
 	return &ConquestProvider{
 		ProviderData: p,
-		Tenant:       "common",
+		// Tenant:       "common",
 	}
 }
 
 // Configure defaults the ConquestProvider configuration options
 func (p *ConquestProvider) Configure(tenant string) {
-	if tenant == "" || tenant == "common" {
-		// tenant is empty or default, remain on the default "common" tenant
-		return
-	}
+	// if tenant == "" || tenant == "common" {
+	// 	// tenant is empty or default, remain on the default "common" tenant
+	// 	return
+	// }
 
 	// Specific tennant specified, override the Login and RedeemURLs
-	p.Tenant = tenant
-	overrideTenantURL(p.LoginURL, conquestDefaultLoginURL, tenant, "authorize")
-	overrideTenantURL(p.RedeemURL, conquestDefaultRedeemURL, tenant, "token")
+	// p.Tenant = tenant
+	// overrideTenantURL(p.LoginURL, conquestDefaultLoginURL, tenant, "authorize")
+	// overrideTenantURL(p.RedeemURL, conquestDefaultRedeemURL, tenant, "token")
 }
 
 // func overrideTenantURL(current, defaultURL *url.URL, tenant, path string) {
@@ -199,6 +199,7 @@ func (p *ConquestProvider) EnrichSession(ctx context.Context, s *sessions.Sessio
 		return errors.New("unable to get email address")
 	}
 	s.Email = email
+	logger.Printf("Found email adress: %v", email)
 
 	return nil
 }
@@ -217,7 +218,7 @@ func (p *ConquestProvider) prepareRedeem(redirectURL, code string) (url.Values, 
 	params.Add("client_id", p.ClientID)
 	params.Add("client_secret", clientSecret)
 	params.Add("code", code)
-	params.Add("response_type", "authorization_code")
+	params.Add("grant_type", "authorization_code")
 	// if p.ProtectedResource != nil && p.ProtectedResource.String() != "" {
 	// 	params.Add("resource", p.ProtectedResource.String())
 	// }
@@ -233,6 +234,7 @@ func (p *ConquestProvider) verifyTokenAndExtractEmail(ctx context.Context, token
 		token, err := p.Verifier.Verify(ctx, token)
 		// due to issues mentioned above, id_token may not be signed by AAD
 		if err == nil {
+			logger.Printf("Got token, getting claims")
 			claims, err := p.getClaims(token)
 			if err == nil {
 				email = claims.Email
@@ -326,30 +328,30 @@ func makeConquestHeader(accessToken string) http.Header {
 	return makeAuthorizationHeader(tokenTypeBearer, accessToken, nil)
 }
 
-// func getEmailFromJSON(json *simplejson.Json) (string, error) {
-// 	var email string
-// 	var err error
+func getSessionEmailFromJSON(json *simplejson.Json) (string, error) {
+	var email string
+	var err error
 
-// 	email, err = json.Get("mail").String()
+	email, err = json.Get("preferred_username").String()
 
-// 	if err != nil || email == "" {
-// 		otherMails, otherMailsErr := json.Get("otherMails").Array()
-// 		if len(otherMails) > 0 {
-// 			email = otherMails[0].(string)
-// 		}
-// 		err = otherMailsErr
-// 	}
+	if err != nil || email == "" {
+		otherMails, otherMailsErr := json.Get("otherMails").Array()
+		if len(otherMails) > 0 {
+			email = otherMails[0].(string)
+		}
+		err = otherMailsErr
+	}
 
-// 	if err != nil || email == "" {
-// 		email, err = json.Get("userPrincipalName").String()
-// 		if err != nil {
-// 			logger.Errorf("unable to find userPrincipalName: %s", err)
-// 			return "", err
-// 		}
-// 	}
+	if err != nil || email == "" {
+		email, err = json.Get("userPrincipalName").String()
+		if err != nil {
+			logger.Errorf("unable to find userPrincipalName: %s", err)
+			return "", err
+		}
+	}
 
-// 	return email, err
-// }
+	return email, err
+}
 
 func (p *ConquestProvider) getEmailFromProfileAPI(ctx context.Context, accessToken string) (string, error) {
 	if accessToken == "" {
@@ -365,10 +367,155 @@ func (p *ConquestProvider) getEmailFromProfileAPI(ctx context.Context, accessTok
 		return "", err
 	}
 
-	return getEmailFromJSON(json)
+	return getSessionEmailFromJSON(json)
 }
 
 // ValidateSession validates the AccessToken
 func (p *ConquestProvider) ValidateSession(ctx context.Context, s *sessions.SessionState) bool {
 	return validateToken(ctx, p, s.AccessToken, makeConquestHeader(s.AccessToken))
 }
+
+
+// package providers
+
+// import (
+// 	"bytes"
+// 	"context"
+// 	"errors"
+// 	"fmt"
+// 	"net/url"
+
+// 	"github.com/oauth2-proxy/oauth2-proxy/v7/pkg/apis/middleware"
+// 	"github.com/oauth2-proxy/oauth2-proxy/v7/pkg/apis/sessions"
+// 	"github.com/oauth2-proxy/oauth2-proxy/v7/pkg/requests"
+// )
+
+// var (
+// 	// ErrNotImplemented is returned when a provider did not override a default
+// 	// implementation method that doesn't have sensible defaults
+// 	ErrNotImplemented = errors.New("not implemented")
+
+// 	// ErrMissingCode is returned when a Redeem method is called with an empty
+// 	// code
+// 	ErrMissingCode = errors.New("missing code")
+
+// 	// ErrMissingIDToken is returned when an oidc.Token does not contain the
+// 	// extra `id_token` field for an IDToken.
+// 	ErrMissingIDToken = errors.New("missing id_token")
+
+// 	// ErrMissingOIDCVerifier is returned when a provider didn't set `Verifier`
+// 	// but an attempt to call `Verifier.Verify` was about to be made.
+// 	ErrMissingOIDCVerifier = errors.New("oidc verifier is not configured")
+
+// 	_ Provider = (*ProviderData)(nil)
+// )
+
+// // GetLoginURL with typical oauth parameters
+// func (p *ProviderData) GetLoginURL(redirectURI, state, _ string) string {
+// 	extraParams := url.Values{}
+// 	loginURL := makeLoginURL(p, redirectURI, state, extraParams)
+// 	return loginURL.String()
+// }
+
+// // Redeem provides a default implementation of the OAuth2 token redemption process
+// func (p *ProviderData) Redeem(ctx context.Context, redirectURL, code string) (*sessions.SessionState, error) {
+// 	if code == "" {
+// 		return nil, ErrMissingCode
+// 	}
+// 	clientSecret, err := p.GetClientSecret()
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	params := url.Values{}
+// 	params.Add("redirect_uri", redirectURL)
+// 	params.Add("client_id", p.ClientID)
+// 	params.Add("client_secret", clientSecret)
+// 	params.Add("code", code)
+// 	params.Add("grant_type", "authorization_code")
+// 	if p.ProtectedResource != nil && p.ProtectedResource.String() != "" {
+// 		params.Add("resource", p.ProtectedResource.String())
+// 	}
+
+// 	result := requests.New(p.RedeemURL.String()).
+// 		WithContext(ctx).
+// 		WithMethod("POST").
+// 		WithBody(bytes.NewBufferString(params.Encode())).
+// 		SetHeader("Content-Type", "application/x-www-form-urlencoded").
+// 		Do()
+// 	if result.Error() != nil {
+// 		return nil, result.Error()
+// 	}
+
+// 	// blindly try json and x-www-form-urlencoded
+// 	var jsonResponse struct {
+// 		AccessToken string `json:"access_token"`
+// 	}
+// 	err = result.UnmarshalInto(&jsonResponse)
+// 	if err == nil {
+// 		return &sessions.SessionState{
+// 			AccessToken: jsonResponse.AccessToken,
+// 		}, nil
+// 	}
+
+// 	values, err := url.ParseQuery(string(result.Body()))
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	// TODO (@NickMeves): Uses OAuth `expires_in` to set an expiration
+// 	if token := values.Get("access_token"); token != "" {
+// 		ss := &sessions.SessionState{
+// 			AccessToken: token,
+// 		}
+// 		ss.CreatedAtNow()
+// 		return ss, nil
+// 	}
+
+// 	return nil, fmt.Errorf("no access token found %s", result.Body())
+// }
+
+// // GetEmailAddress returns the Account email address
+// // Deprecated: Migrate to EnrichSession
+// func (p *ProviderData) GetEmailAddress(_ context.Context, _ *sessions.SessionState) (string, error) {
+// 	return "", ErrNotImplemented
+// }
+
+// // EnrichSession is called after Redeem to allow providers to enrich session fields
+// // such as User, Email, Groups with provider specific API calls.
+// func (p *ProviderData) EnrichSession(_ context.Context, _ *sessions.SessionState) error {
+// 	return nil
+// }
+
+// // Authorize performs global authorization on an authenticated session.
+// // This is not used for fine-grained per route authorization rules.
+// func (p *ProviderData) Authorize(_ context.Context, s *sessions.SessionState) (bool, error) {
+// 	if len(p.AllowedGroups) == 0 {
+// 		return true, nil
+// 	}
+
+// 	for _, group := range s.Groups {
+// 		if _, ok := p.AllowedGroups[group]; ok {
+// 			return true, nil
+// 		}
+// 	}
+
+// 	return false, nil
+// }
+
+// // ValidateSession validates the AccessToken
+// func (p *ProviderData) ValidateSession(ctx context.Context, s *sessions.SessionState) bool {
+// 	return validateToken(ctx, p, s.AccessToken, nil)
+// }
+
+// // RefreshSession refreshes the user's session
+// func (p *ProviderData) RefreshSession(_ context.Context, _ *sessions.SessionState) (bool, error) {
+// 	return false, ErrNotImplemented
+// }
+
+// // CreateSessionFromToken converts Bearer IDTokens into sessions
+// func (p *ProviderData) CreateSessionFromToken(ctx context.Context, token string) (*sessions.SessionState, error) {
+// 	if p.Verifier != nil {
+// 		return middleware.CreateTokenToSessionFunc(p.Verifier.Verify)(ctx, token)
+// 	}
+// 	return nil, ErrNotImplemented
+// }
